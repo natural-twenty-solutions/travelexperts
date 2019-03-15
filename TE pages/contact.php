62c146fa01450c0b84923fa9d99ee7863c8dabcf
conniehe <xhe_d@hotmail.com>
2019-03-15 11:58:07 -0600

    Update contact.php

--- TE pages/contact.php
+++ TE pages/contact.php
@@ -61,7 +61,7 @@
     <div class="container">
       <div class="row row-grid align-items-center mb-5">
         <div class="col-lg-6">
-          <h3 class="text-primary font-weight-light mb-2">Thank you for supporting us!</h3>
+          <h3 class="text-primary font-weight-light mb-2">testtest Thank you for supporting us!</h3>
           <h4 class="mb-0 font-weight-light">Let's get in touch on any of these platforms.</h4>
         </div>
         <div class="col-lg-6 text-lg-center btn-wrapper">
