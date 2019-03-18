dcf44b06974a677ec4f9457b96caa42ece999bf4
frederickfernandez-sait <48568688+frederickfernandez-sait@users.noreply.github.com>
2019-03-18 14:27:42 -0600

    Missing from shopping car: -Individual entries for package. When you add packages, it all populates one item entry -Remove item button does not work

--- TE pages/cart1.php
+++ TE pages/cart1.php
@@ -6,15 +6,20 @@
 
   if(!empty($_GET["action"])) {
   switch($_GET["action"]) {
+
   	case "add":
   		if(!empty($_POST["quantity"])) {
-  			$productByCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='" . $_GET["code"] . "'");
-  			$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode[0]["price"], 'image'=>$productByCode[0]["image"]));
+  			$productByCode = $db_handle->runQuery("SELECT * FROM packages");
+  			$itemArray = array($productByCode[0]["PackageId"]=>array('PkgName'=>$productByCode[0]["PkgName"],
+        'PackageId'=>$productByCode[0]["PackageId"],
+        'quantity'=>$_POST["quantity"],
+        'PkgBasePrice'=>$productByCode[0]["PkgBasePrice"],
+        'image'=>$productByCode[0]["image"]));
 
   			if(!empty($_SESSION["cart_item"])) {
-  				if(in_array($productByCode[0]["code"],array_keys($_SESSION["cart_item"]))) {
+  				if(in_array($productByCode[0]["PackageId"],array_keys($_SESSION["cart_item"]))) {
   					foreach($_SESSION["cart_item"] as $k => $v) {
-  							if($productByCode[0]["code"] == $k) {
+  							if($productByCode[0]["PackageId"] == $k) {
   								if(empty($_SESSION["cart_item"][$k]["quantity"])) {
   									$_SESSION["cart_item"][$k]["quantity"] = 0;
   								}
@@ -29,16 +34,18 @@
   			}
   		}
   	break;
+
   	case "remove":
   		if(!empty($_SESSION["cart_item"])) {
   			foreach($_SESSION["cart_item"] as $k => $v) {
-  					if($_GET["code"] == $k)
+  					if($_GET["PackageId"] == $k)
   						unset($_SESSION["cart_item"][$k]);
   					if(empty($_SESSION["cart_item"]))
   						unset($_SESSION["cart_item"]);
   			}
   		}
   	break;
+
   	case "empty":
   		unset($_SESSION["cart_item"]);
   	break;
@@ -54,15 +61,17 @@
     </section>
   </div>
 
-  <div id="shopping-cart">
+<div id="shopping-cart">
   <div class="txt-heading">Shopping Cart</div>
 
   <a id="btnEmpty" href="cart1.php?action=empty">Empty Cart</a>
+
   <?php
   if(isset($_SESSION["cart_item"])){
      $total_quantity = 0;
      $total_price = 0;
   ?>
+
   <table class="tbl-cart" cellpadding="10" cellspacing="1">
   <tbody>
   <tr>
@@ -74,22 +83,22 @@
   </tr>
 
   <?php
-     foreach ($_SESSION["cart_item"] as $item){
-         $item_price = $item["quantity"]*$item["price"];
-         ?>
-   				<tr>
-   				<td><img src="<?php echo $item["image"]; ?>" class="cart-item-image" /><?php echo $item["name"]; ?></td>
-   				<td><?php echo $item["code"]; ?></td>
-   				<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
-   				<td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
-   				<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
-   				<td style="text-align:center;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
-   				</tr>
-   				<?php
-   				$total_quantity += $item["quantity"];
-   				$total_price += ($item["price"]*$item["quantity"]);
-   		}
-   		?>
+  foreach ($_SESSION["cart_item"] as $item){
+    $item_price = $item["quantity"]*$item["PkgBasePrice"];
+  ?>
+      <tr>
+      <td><img src="<?php echo $item["image"]; ?>" class="cart-item-image" /><?php echo $item["PkgName"]; ?></td>
+      <td><?php echo $item["PackageId"]; ?></td>
+      <td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
+      <td  style="text-align:right;"><?php echo "$ ".$item["PkgBasePrice"]; ?></td>
+      <td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
+      <td style="text-align:center;"><a href="cart1.php?action=remove&code=<?php echo $item["PackageId"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+      </tr>
+  <?php
+    	$total_quantity += $item["quantity"];
+    	$total_price += ($item["PkgBasePrice"]*$item["quantity"]);
+  }
+  ?>
 
 
   <tr>
@@ -110,27 +119,27 @@
   </div>
 
   <div id="product-grid">
-    <div class="txt-heading">Products</div>
-    <?php
-    $product_array = $db_handle->runQuery("SELECT * FROM packages");
-    if (!empty($product_array)) {
-     foreach($product_array as $key=>$value){
-    ?>
-     <div class="product-item">
-       <form method="post" action="cart1.php?action=add&code=<?php echo $product_array[$key]["PackageId"]; ?>">
-       <div class="product-image"><img src="<?php echo $product_array[$key]["image"]; ?>"></div>
-       <div class="product-tile-footer">
-       <div class="product-title"><?php echo $product_array[$key]["PkgName"]; ?></div>
-       <div class="product-price"><?php echo "$".$product_array[$key]["PkgBasePrice"]; ?></div>
-       <div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
-       </div>
-       </form>
-     </div>
+  <div class="txt-heading">Products</div>
+  <?php
+  $product_array = $db_handle->runQuery("SELECT * FROM packages");
+  if (!empty($product_array)) {
+  foreach($product_array as $key=>$value){
+  ?>
+  <div class="product-item" align="center">
+    <form method="post" action="cart1.php?action=add&code=<?php echo $product_array[$key]["PackageId"]; ?>">
+    <div class="product-image"><img height="100%" width="100%" src="<?php echo $product_array[$key]["image"]; ?>"></div>
+    <div class="product-tile-footer">
+    <div class="product-title"><?php echo $product_array[$key]["PkgName"]; ?></div>
+    <div class="product-price"><?php echo "$".$product_array[$key]["PkgBasePrice"]; ?></div>
+    <div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
+    </div>
+   </form>
+  </div>
     <?php
      }
     }
     ?>
-  </div>
+</div>
 
 
  </body>
