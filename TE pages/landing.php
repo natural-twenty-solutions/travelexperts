0775f0a1f801272fbf5c8fe354b1d9a9c00900ec
frederickfernandez-sait <48568688+frederickfernandez-sait@users.noreply.github.com>
2019-03-19 11:34:58 -0600

    merging

--- TE pages/landing.php
+++ TE pages/landing.php
@@ -4,23 +4,10 @@
   $hour = date("H");
  ?>
 
-  <main>
+ <main>
     <div class="position-relative">
-
-      <section class="section section-lg section-shaped pb-50">
-        <div class="shape shape-style-1 shape-default">
-          <span class="span-150"></span>
-          <span class="span-50"></span>
-          <span class="span-50"></span>
-          <span class="span-75"></span>
-          <span class="span-100"></span>
-          <span class="span-75"></span>
-          <span class="span-50"></span>
-          <span class="span-100"></span>
-          <span class="span-50"></span>
-          <span class="span-100"></span>
-        </div>
-
+    <section class="section section-lg section-shaped pb-50">
+      <div class="shape shape-style-2 shape-default"></div>
         <div class="container py-lg-md d-flex">
           <div class="col px-0">
             <div class="row">
@@ -39,9 +26,7 @@
                     }
                    ?></b>
                 </h1>
-                <h1 class="display-3  text-white">Welcome to Travel Experts
-
-                </h1>
+                <h1 class="display-3  text-white">Welcome to Travel Experts</h1>
                 <p class="lead  text-white">Book your next travel, vacation, or cruise with us. We share your passion for exploration, your love of culture and your excitement in discovering new lands. </p>
                 <div class="btn-wrapper">
                   <a href="..\TE pages\package.php" target="_blank" class="btn btn-white btn-icon mb-3 mb-sm-0">
@@ -54,69 +39,59 @@
                   </a>
                 </div>
               </div>
-
-              <div class="col-lg-6">
-
-                <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
-                  <ol class="carousel-indicators">
-                    <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="1" class=""></li>
-                    <li data-target="#carouselIndicators" data-slide-to="2" class=""></li>
-                    <li data-target="#carouselIndicators" data-slide-to="3" class=""></li>
-                  </ol>
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img class="c-block w-100" src="./IMG/asian1.jpg" style=height:350px; alt="First slide">
-                        <div class="carousel-caption">
-                          <h3 style=color:white;>Asian Expedition</h3>
-                          <p>Vietnam, Thailand, and more!</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" src="./IMG/carib1.jpg" style=height:350px; alt="Second slide">
-                        <div class="carousel-caption">
-                          <h3 style=color:white;>Caribbean New Year</h3>
-                          <p>Celebrate New Years in sun!</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" src="./IMG/europe1.jpg" style=height:350px; alt="Third slide">
-                        <div class="carousel-caption">
-                          <h3 style=color:white;>European Vacation</h3>
-                          <p>Historic and memorable!</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" src="./IMG/poly1.jpg" style=height:350px; alt="Fourth slide">
-                        <div class="carousel-caption">
-                          <h3 style=color:white;>Polynesian Paradise</h3>
-                          <p>Soak in the sun while relaxing on a beach!</p>
-                        </div>
-                    </div>
+            <div class="col-lg-6">
+              <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselIndicators" data-slide-to="1" class=""></li>
+                  <li data-target="#carouselIndicators" data-slide-to="2" class=""></li>
+                  <li data-target="#carouselIndicators" data-slide-to="3" class=""></li>
+                </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="c-block w-100" src="./IMG/asian1.jpg" style=height:350px; alt="First slide">
+                    <div class="carousel-caption">
+                      <h3 style=color:white;>Asian Expedition</h3>
+                      <p>Vietnam, Thailand, and more!</p>
                   </div>
-                  <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                  <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                  </a>
-                </div>
-
-                <!-- <div class="card bg-default shadow border-0">
-                  <img src="../IMG/poly2.jpg" class="card-img-top">
-                </div> -->
-
               </div>
-
+              <div class="carousel-item">
+                <img class="d-block w-100" src="./IMG/carib1.jpg" style=height:350px; alt="Second slide">
+                  <div class="carousel-caption">
+                    <h3 style=color:white;>Caribbean New Year</h3>
+                    <p>Celebrate New Years in sun!</p>
+                  </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="./IMG/europe1.jpg" style=height:350px; alt="Third slide">
+                  <div class="carousel-caption">
+                    <h3 style=color:white;>European Vacation</h3>
+                    <p>Historic and memorable!</p>
+                  </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="./IMG/poly1.jpg" style=height:350px; alt="Fourth slide">
+                  <div class="carousel-caption">
+                    <h3 style=color:white;>Polynesian Paradise</h3>
+                    <p>Soak in the sun while relaxing on a beach!</p>
+                  </div>
+                </div>
+            </div>
+          <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <!-- 1st Hero Variation -->
-    </div>
-
+        </section>
+      </div>
 
     <section class="section bg-secondary">
       <div class="container">
@@ -144,9 +119,8 @@
           </div>
         </div>
       </div>
-    </br></br></br></br></br></br></br></br></br></br>
+      </br></br></br></br></br></br></br></br></br></br>
     </section>
-
   </main>
 
 <?php
