66f5e2d971918e36503ae0a166cf2f9dab773abe
heatherjp <39212764+heatherjp@users.noreply.github.com>
2019-03-20 14:07:57 -0600

    payment update

--- TE pages/payment.php
+++ TE pages/payment.php
@@ -4,7 +4,7 @@
  ?>
   <main class="profile-page">
 
-    <section class="section-profile-cover section-shaped my-0">
+    <section class="section section-lg section-hero section-shaped">
       <!-- Circles background -->
       <div class="shape shape-style-1 shape-primary alpha-4">
         <span></span>
@@ -15,28 +15,21 @@
         <span></span>
         <span></span>
       </div>
-      <!-- SVG separator -->
-      <div class="separator separator-bottom separator-skew">
-        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
-          <polygon class="fill-white" points="2560 0 2560 100 0 100"></polygon>
-        </svg>
-      </div>
-    </section>
-        <section class="section section-lg pt-lg-0 section-contact-us">
+
           <div class="container pt-lg-md">
-            <div class="row justify-content-center mt--300">
+            <div class="row justify-content-center">
               <div class="col-lg-8">
                 <div class="card bg-gradient-secondary shadow">
                   <div class="card-body p-lg-5">
-                    <h4 class="display-4">Travel Experts Order Summary</h4><hr>
-
-                    <table  cellpadding="10" cellspacing="1">
-                      <tbody>
+                    <h4 class="display-4">Travel Experts Order Summary</h4><br>
+                    <div class="table-responsive">
+                    <table  class="table align-items-center table-flush">
+                      <thead class="thead-light">
                         <tr>
-                        <th style="text-align:left;" width="30%">Name</th>
-                        <th style="text-align:right;" width="20%">Quantity</th>
-                        <th style="text-align:right;" width="25%">Unit Price</th>
-                        <th style="text-align:right;" width="25%">Price</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Unit Price</th>
+                        <th scope="col">Price</th>
                         </tr>
 
                         <?php
@@ -46,18 +39,25 @@
                         ?>
 
                         <tr>
-                        <td style="text-align:left;"><?php echo $item["name"]; ?></td>
-                        <td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
-                        <td style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
-                        <td style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
+                        <td scope="col"><?php echo $item["name"]; ?></td>
+                        <td scope="col"><?php echo $item["quantity"]; ?></td>
+                        <td scope="col"><?php echo "$ ".$item["price"]; ?></td>
+                        <td scope="col"><?php echo "$ ". number_format($item_price,2); ?></td>
                         </tr>
 
+                        <tr>
+                          <td></td>
+                          <td></td>
+                        <td scope="col"><b>Total: </b></td>
+
+                        <td scope="col" ><b>$
+                        <?php    print $_SESSION['total_price'];?></b></td>
+
+                        </tr>
                       </tbody>
                     </table>
-                    <br>
-                    <p class="heading 4" style="text-align:center;"> Your Total is $
-                      <?php    print $_SESSION['total_price'];?>
-                    </p><br><br>
+                  </div>
+                  <hr>
 
 <!-- YOUR ORDER
                     <div class="row">
@@ -68,7 +68,7 @@
                     </div><br>-->
 
 <!-- row1 -->
-<h4 class="display-4">Payment Method</h4><hr>
+<h4 class="display-4">Payment Method</h4><br><br>
                     <div class="row">
                       <div class="col">
                         <label for="cname">Full Name</label>
